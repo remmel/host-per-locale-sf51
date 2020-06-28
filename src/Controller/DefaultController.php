94d9426abbcc352160fbd993147c095052863d09
Remy Mellet <remy.mellet@gmail.com>
2020-06-28 02:03:17 +0200

    add controller with 2 pages (home + contact)

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('default/contact.html.twig');
+    }
+}
